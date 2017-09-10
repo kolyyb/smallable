47bc6f56bc37c8320963f59532c2c558bb47626d
Karine Pham <kpham@k-ph.fr>
2017-09-10 13:23:59 +0100

    video background + search-bar + responsive

---
+++ app.js
@@ -0,0 +1,36 @@
+'use strict'
+
+// Contrôle de la lecture de la video par l'internaute
+
+var btn = document.getElementById("btn-control");
+var video = document.getElementById('video');
+
+btn.addEventListener('click', function() {
+    if(video.paused) {
+        video.play();
+    } else {
+        video.pause();
+    }
+});
+
+// Contrôle de la boucle de la video
+
+var count = 1;
+video.addEventListener('ended', function() {
+    if(count < 3) {
+        this.currentTime = 0;
+        this.play();
+        count++
+    }
+});
+
+// Gestion du menu pour tablettes et mobiles
+
+var hamb_menu = document.getElementById('hamb-menu');
+var nav_content = document.getElementById('menu-deploy');
+
+hamb_menu.addEventListener('click', function() {
+    nav_content.classList.toggle('show-menu');
+    console.log('Texte affiché : fonction opé.');
+    console.log(nav_content.classList);
+})
